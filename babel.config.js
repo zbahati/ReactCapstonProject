b25a811fe8f0b873e81602c8487428e94c8f3629
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-06 17:30:45 +0300

    update test

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
   ],
 };
