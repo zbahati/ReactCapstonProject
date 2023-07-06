87329b4b8ac9152ef8467dd9b0ca96c0c7f9ddda
ANTOINE1128 <makdessyantoine@gmail.com>
2023-07-06 17:32:15 +0300

    fix linters

--- src/__test__/DetailsPage.test.js
+++ src/__test__/DetailsPage.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CountriesDetails from '../components/main/CountriesDetails';
 import { Provider } from 'react-redux';
+import CountriesDetails from '../components/main/CountriesDetails';
 import store from '../redux/store';
 
 test('DetailsPage matches snapshot', () => {
   const { container } = render(
     <Provider store={store}>
-       <Router>
+      <Router>
         <CountriesDetails />
       </Router>
-    </Provider>
+    </Provider>,
 
   );
   expect(container).toMatchSnapshot();
